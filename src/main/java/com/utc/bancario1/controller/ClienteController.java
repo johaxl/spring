e253f3e47dd99aab96d493cdbf4db488b074f0d3
johaxl <johan.lozada5793@utc.edu.ec>
2026-01-27 11:35:56 -0500

    Base de datos actualizada

--- src/main/java/com/utc/bancario1/controller/ClienteController.java
+++ src/main/java/com/utc/bancario1/controller/ClienteController.java
@@ -38,11 +38,11 @@ public class ClienteController {
     public String guardar(@ModelAttribute Cliente c, RedirectAttributes ra) {
         if(c.getId() == null){
             service.guardar(c);
-            ra.addFlashAttribute("mensaje","Cliente registrado correctamente");
+            ra.addFlashAttribute("mensaje","Successfully registered customer");
             ra.addFlashAttribute("tipo","success");
         } else {
             service.guardar(c);
-            ra.addFlashAttribute("mensaje","Cliente actualizado correctamente");
+            ra.addFlashAttribute("mensaje","Successfully updated client");
             ra.addFlashAttribute("tipo","info");
         }
         return "redirect:/clientes";
@@ -52,7 +52,7 @@ public class ClienteController {
     public String eliminar(@PathVariable Long id, RedirectAttributes ra){
         Cliente c = service.buscarPorId(id);
         if(c.getCuentas() != null && !c.getCuentas().isEmpty()){
-            ra.addFlashAttribute("mensaje","No se puede eliminar este cliente porque tiene cuentas asociadas");
+            ra.addFlashAttribute("mensaje","This customer cannot be deleted because it has associated accounts");
             ra.addFlashAttribute("tipo","warning");
             return "redirect:/clientes";
         }
