3b0135ed4bc1195e3cfa11f12c6d3da945b39a05
johaxl <johan.lozada5793@utc.edu.ec>
2026-01-27 08:07:39 -0500

    Cambios en el html

--- src/main/java/com/utc/bancario1/controller/ClienteController.java
+++ src/main/java/com/utc/bancario1/controller/ClienteController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.utc.bancario1.entity.Cliente;
 import com.utc.bancario1.service.ClienteService;
@@ -15,34 +16,52 @@ public class ClienteController {
     @Autowired
     private ClienteService service;
 
-    // LISTAR
+    // INDEX
     @GetMapping
-    public String listar(Model model) {
-        model.addAttribute("cliente", new Cliente());
+    public String index(Model model) {
         model.addAttribute("listaClientes", service.listar());
-        return "cliente";
+        return "clientes/index";
     }
 
-    // GUARDAR / ACTUALIZAR
+    // FORM NUEVO
+    @GetMapping("/nuevo")
+    public String nuevo(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "clientes/agregar";
+    }
+
+    // FORM EDITAR
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        model.addAttribute("cliente", service.buscarPorId(id));
+        return "clientes/editar";
+    }
+
+    // GUARDAR (nuevo y editar)
     @PostMapping("/guardar")
-    public String guardar(@ModelAttribute Cliente c) {
-        service.guardar(c); // si tiene ID = actualiza
+    public String guardar(@ModelAttribute Cliente c, RedirectAttributes ra) {
+
+        if (c.getId() == null) {
+            service.guardar(c);
+            ra.addFlashAttribute("mensaje", "Cliente registrado correctamente");
+            ra.addFlashAttribute("tipo", "success");
+        } else {
+            service.guardar(c);
+            ra.addFlashAttribute("mensaje", "Cliente actualizado correctamente");
+            ra.addFlashAttribute("tipo", "info");
+        }
+
         return "redirect:/clientes";
     }
 
-    // EDITAR
-    @GetMapping("/editar/{id}")
-    public String editar(@PathVariable Long id, Model model) {
-        Cliente c = service.buscarPorId(id);
-        model.addAttribute("cliente", c);
-        model.addAttribute("listaClientes", service.listar());
-        return "cliente";
-    }
+
 
     // ELIMINAR
     @GetMapping("/eliminar/{id}")
-    public String eliminar(@PathVariable Long id) {
+    public String eliminar(@PathVariable Long id, RedirectAttributes ra) {
         service.eliminar(id);
+        ra.addFlashAttribute("mensaje", "Cliente eliminado correctamente");
+        ra.addFlashAttribute("tipo", "success");
         return "redirect:/clientes";
     }
 }
