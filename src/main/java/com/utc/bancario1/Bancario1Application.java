a59ae0de09ff13404ae0e682ec19d8cce9f10000
johaxl <johan.lozada5793@utc.edu.ec>
2026-01-29 11:56:50 -0500

    Subir a red hat

--- src/main/java/com/utc/bancario1/Bancario1Application.java
+++ src/main/java/com/utc/bancario1/Bancario1Application.java
@@ -2,12 +2,19 @@ package com.utc.bancario1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Bancario1Application {
+public class Bancario1Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Bancario1Application.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Bancario1Application.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Bancario1Application.class, args);
+    }
 }
+
